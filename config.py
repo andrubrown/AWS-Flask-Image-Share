804e77dee05f092aba9a4201ccd211192107fcc9
Daeyun Shin <daeyunshin@gmail.com>
2014-04-12 22:41:36 -0500

    Re-commit config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+AWS_ACCESS_KEY_ID = 'your aws access key id'
+AWS_SECRET_ACCESS_KEY = 'your aws access key'
+AWS_STORAGE_BUCKET_NAME = AWS_ACCESS_KEY_ID.lower() + '_user_img'
+
+# Elastic Bealstalk environment variable will override this.
+AWS_DYNAMO_TABLE = 'your dynamodb table name'
